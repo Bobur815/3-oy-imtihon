34d0676485ce62e41da7483e21235af7ed64dc39
Boburmirzo Ergashev <ergashevboburmirzo815@gmail.com>
2025-05-29 16:45:32 +0500

    updated

--- src/middleware/checktoken.js
+++ src/middleware/checktoken.js
@@ -4,8 +4,9 @@ import jwt from "../utils/jwt.js";
 
 export default async (req,res,next) => {
     try {
-        const token =  req.headers.authorization || req.headers.token
-        console.log(token);
+
+        const token =  req.headers.authorization.split(" ")[1] || req.headers.token
+
         if (!token) {
             throw new CustomError("Token is required!",404, "NotFoundError");
         }
